530f369e2022e475acca8d3e278d7919ec51945f
Plextora <71889427+Plextora@users.noreply.github.com>
2022-11-23 21:00:24 -0500

    Added some misc cli stuff lol

--- Program.cs
+++ Program.cs
@@ -4,14 +4,47 @@
 using CommandLine;
 using CommandLine.Text;
 using KrnlAPI;
+using System.Collections.Generic;
+using System;
 using WeAreDevs_API;
 
 namespace CLIsploit
 {
     internal class Program
     {
+        public class Options
+        {
+            [Option("api", Required = true, HelpText = "Specify which exploit API you want to use")]
+            public string api { get; set; }
+        }
+
+        static void DisplayHelp<T>(ParserResult<T> result, IEnumerable<Error> errs)
+        {
+            var helpText = HelpText.AutoBuild(result, h =>
+            {
+                h.AdditionalNewLineAfterOption = false;
+                h.Heading = "CLIsploit - Exploit in ROBLOX the chad way";
+                h.Copyright =
+                    "Copyright 2022 Plextora\n\n" +
+                    "This program comes with ABSOLUTELY NO WARRANTY.\n" +
+                    "This is free (as in freedom) software, and you are welcome to redistribute it under certain conditions.\n" +
+                    "Visit <https://www.gnu.org/licenses/gpl-3.0.en.html> for more information.";
+                return HelpText.DefaultParsingErrorsHandler(result, h);
+            }, e => e);
+            Console.WriteLine(helpText);
+        }
+
         static void Main(string[] args)
         {
+            var parser = new CommandLine.Parser(with => with.HelpWriter = null);
+            var parserResult = parser.ParseArguments<Options>(args);
+            parserResult
+                .WithParsed<Options>(options => Run(options))
+                .WithNotParsed(errs => DisplayHelp(parserResult, errs));
         }
+
+        public static void Run(Options options) {}
+
+        public static void Inject(string api) {}
     }
 }
